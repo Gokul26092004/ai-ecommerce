2dedb510749410eb6d8f5a727cc1e79078c5caaa
Gokul26092004 <gokulm2609@gmail.com>
2026-03-02 18:08:54 +0530

    Day 16 Visual search using Mobilenet was successfully done

--- app/app.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from recommender import recommend
-
-app = FastAPI()
-
-@app.get("/recommend/{product_id}")
-def get_recommendation(product_id:int):
-    return recommend(product_id)
